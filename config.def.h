efb0b380d47847ed22e367ab7f8195f3a7b13fe4
Christian <shadow2ube@users.noreply.github.com>
2022-02-23 13:56:45 -0500

    dw

--- config.def.h
+++ config.def.h
@@ -64,6 +64,11 @@ static const char unknown_str[] = "n/a";
  * wifi_essid          WiFi ESSID                      interface name (wlan0)
  */
 static const struct arg args[] = {
-	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+	/* function      format       argument */
+	{ battery_state,     "[%s",       "BAT0"  },
+	{ battery_perc,      "%s ",       "BAT0"  },
+	{ battery_remaining, "(%s)] | ",    "BAT0"  },
+	{ ram_free,          "[%s/",      NULL    },
+	{ ram_total,         "%s] | ",    NULL    },
+	{ datetime,          "[%s]",      "%F %T" },
 };
